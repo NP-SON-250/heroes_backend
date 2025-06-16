7c84119b559b8635e11f52cd6493407c404357ab
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-06-16 19:03:45 +0200

    add verify password

--- src/routes/Heroes.users.routes.js
+++ src/routes/Heroes.users.routes.js
@@ -9,11 +9,26 @@ import {
   createUsers,
 } from "../controllers/Heroes.users.controllers";
 import fileUpload from "../helper/multer";
-
+import Users from "../models/Heroes.users.model";
 const userRoute = express.Router();
 userRoute.post("/", fileUpload.single("profile"), createUsers);
 userRoute.post("/auth", fileUpload.single("password"), login);
 userRoute.post("/auth/school", fileUpload.single("password"), loginSchools);
+userRoute.post("/verify-password", normal, async (req, res) => {
+  try {
+    const userId = req.loggedInUser.id;
+    const user = await Users.findById(userId);
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid password" });
+    }
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 userRoute.put("/:id", fileUpload.single("profile"), updateUser);
 userRoute.delete("/:id", deleteUser);
 userRoute.get("/", getAllUsers);
